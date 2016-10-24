216851940632691a227e208696d5b4ddea531800
renevanderark <r.van.der.ark@gmail.com>
2016-10-24 13:56:16 +0200

    initial.

---
+++ generate-bia-mapping.js
@@ -0,0 +1,168 @@
+import http from "http";
+import mappingToJsonLdRml from "./mappingToJsonLdRml";
+
+const archetypes = {
+  concepts: [],
+  locations: [],
+  collectives: [],
+  persons: []
+};
+
+/**
+ * Mappings based on 2016_10_06_BIA_Master_WERFILE_JAUCO.xlsx
+ * these core mappings include the sheets:
+ * - Persons    (archetype: persons)
+ * - Places     (archetype: locations)
+ * - Institutes (archetype: collectives)
+ *
+ * and maps these one to many relations:
+ * - Institute locatedAt Place
+ * - Person hasBirthPlace Place
+ * - Person hasDeathPlace Place
+ */
+const mappings = {
+  collections: {
+    Persons: {
+      archetypeName: "persons",
+      mappings: [
+        {
+          property: "familyName",
+          variable: [{ variableName: "family_name" }]
+        },
+        {
+          property: "givenName",
+          variable: [{ variableName: "given_name" }]
+        },
+        {
+          property: "preposition",
+          variable: [ { variableName: "preposition" }]
+        },
+        {
+          property: "intraposition",
+          variable: [ { variableName: "intraposition" }]
+        },
+        {
+          property: "postposition",
+          variable: [ { variableName: "postposition" }]
+        },
+        {
+          property: "gender",
+          variable: [ { variableName: "gender" }]
+        },
+        {
+          property: "birthDate",
+          variable: [ { variableName: "birth_date" }]
+        },
+        {
+          property: "hasBirthPlace",
+          variable: [{
+            variableName: "birth_place_persistent_id",
+            targetCollection: "Places",
+            targetVariableName: "persistent_id"
+          }]
+        },
+        {
+          property: "deathDate",
+          variable: [ { variableName: "death_date" }]
+        },
+        {
+          property: "hasDeathPlace",
+          variable: [{
+            variableName: "death_place_persistent_id",
+            targetCollection: "Places",
+            targetVariableName: "persistent_id"
+          }]
+        },
+        {
+          property: "religion",
+          variable: [ { variableName: "religion" }]
+        },
+        {
+          property: "originDb",
+          variable: [ { variableName: "origin_db" }]
+        },
+        {
+          property: "important",
+          variable: [ { variableName: "important" }]
+        }
+      ]
+    },
+    Institutes: {
+      archetypeName: "collectives",
+      mappings: [
+        {
+          property: "name",
+          variable: [{
+            variableName: "name"
+          }]
+        }, {
+          property: "locatedAt",
+          variable: [{
+            variableName: "place_persistant_id",
+            targetCollection: "Places",
+            targetVariableName: "persistent_id"
+          }]
+        }
+      ]
+    },
+    Places: {
+      archetypeName: "locations",
+      mappings: [
+        {
+          property: "name",
+          variable: [{ variableName: "name" }]
+        },
+        {
+          property: "country",
+          variable: [{ variableName: "country" }]
+        },
+        {
+          property: "latitude",
+          variable: [{ variableName: "latitude" }]
+        },
+        {
+          property: "longitude",
+          variable: [{ variableName: "longitude" }]
+        },
+        {
+          property: "remarks",
+          variable: [{ variableName: "remarks" }]
+        },
+      ]
+    }
+  }
+};
+const vre = process.env.VRE_ID;
+
+const jsonLd = JSON.stringify(mappingToJsonLdRml(mappings, vre, archetypes));
+
+console.log("JSON LD:\n===========");
+console.log(JSON.stringify(JSON.parse(jsonLd), null, '  '));
+console.log("===========");
+
+
+if (process.env.HOST && process.env.AUTH_HEADER) {
+  const auth = process.env.AUTH_HEADER;
+  const host = process.env.HOST;
+  const port = process.env.PORT || "80";
+  const httpOpts = {
+    host: host,
+    port: port,
+    path: `/v2.1/bulk-upload/${vre}/rml/execute`,
+    method: "POST",
+    headers: {
+      'Authorization': auth,
+      'Content-type': "application/ld+json",
+      'Content-length': Buffer.byteLength(jsonLd)
+    }
+  };
+
+  const req = http.request(httpOpts, (response) => {
+    let str = '';
+    response.on('data', (chunk) => str += chunk);
+    response.on('end', () => console.log(str));
+  });
+
+  req.write(jsonLd);
+  req.end();
+}
---
+++ mappingToJsonLdRml.js
@@ -0,0 +1,114 @@
+export default function mappingToJsonLdRml(mapping, vre, archetypes) {
+  const relationsToExisting = Object.keys(mapping.collections)
+    .map((key) => mapping.collections[key].mappings)
+    .reduce((a, b) => a.concat(b))
+    .filter((columnMapping) => typeof columnMapping.variable[0].targetExistingTimbuctooVre !== "undefined")
+    .map((columnMapping) => columnMapping.property);
+
+  const mappedArchetypes = Object.keys(mapping.collections)
+    .map((key) => mapping.collections[key].archetypeName);
+  
+  const desiredArchetypes = Object.keys(archetypes)
+    .map((key) => archetypes[key])
+    .map((properties) => properties.filter((prop) => prop.type === "relation"))
+    .reduce((a,b) => a.concat(b))
+    .filter((relationType) => relationsToExisting.indexOf(relationType.name) > -1)
+    .map((relationType) => relationType.relation.targetCollection);
+
+  console.log("There are these relationTypes mapped to existing datasets", relationsToExisting);
+  console.log("They need these archetypes to work", desiredArchetypes);
+  console.log("And we actually mapped these archetypes", mappedArchetypes);
+
+  const missingArchetypes = desiredArchetypes
+    .filter((da) => mappedArchetypes.indexOf(da) < 0);
+
+  console.log("So we need to somehow spoof these missing archetypes", missingArchetypes);
+
+  console.log("It needs to look like this",
+   Object.keys(mapping.collections).map(key => mapping.collections[key])[0]);
+
+
+  return {
+  	"@context": {
+  		"@vocab": "http://www.w3.org/ns/r2rml#",
+  		"rml": "http://semweb.mmlab.be/ns/rml#",
+  		"tim": "http://timbuctoo.com/mapping#",
+      "http://www.w3.org/2000/01/rdf-schema#subClassOf": {
+        "@type": "@id"
+      },
+  		"predicate": {
+  			"@type": "@id"
+  		},
+      "termType": {
+        "@type": "@id"
+      },
+      "parentTriplesMap": {
+        "@type": "@id"
+      },
+      "class": {
+        "@type": "@id"
+      }
+  	},
+  	"@graph": Object.keys(mapping.collections)
+      .map(key => mapSheet(key, mapping.collections[key], vre))
+      .concat(missingArchetypes.map((archetypeName) => (
+        {archetypeName: archetypeName, customProperties: [], mappings: [], ignoredColumns: []}
+      )).map((mapping) => mapSheet(mapping.archetypeName, mapping, vre)))
+  };
+}
+
+function makeMapName(vre, localName) {
+  return `http://timbuctoo.com/mapping/${vre}/${localName}`;
+}
+
+function mapSheet(key, sheet, vre) {
+  //FIXME: move logicalSource and subjectMap under the control of the server
+  return {
+    "@id": makeMapName(vre, key),
+    "http://www.w3.org/2000/01/rdf-schema#subClassOf": `http://timbuctoo.com/${sheet.archetypeName.replace(/s$/, "")}`,
+    "rml:logicalSource": {
+			"rml:source": {
+				"tim:rawCollection": key,
+				"tim:vreName": vre
+			}
+		},
+    "subjectMap": {
+			"class": makeMapName(vre, key),
+			"template": `${makeMapName(vre, key)}/{tim_id}`
+		},
+    "predicateObjectMap": sheet.mappings.map(makePredicateObjectMap.bind(null, vre))
+  };
+}
+
+function makePredicateObjectMap(vre, mapping) {
+  let property = mapping.property;
+  let variable = mapping.variable[0];
+  if (variable.targetCollection) {
+    return {
+      "objectMap": {
+        "joinCondition": {
+          "child": variable.variableName,
+          "parent": variable.targetVariableName
+        },
+        "parentTriplesMap": makeMapName(vre, variable.targetCollection)
+      },
+      "predicate": `http://timbuctoo.com/${property}`
+    }
+  } else if (variable.targetExistingTimbuctooVre) {
+    return {
+      "objectMap": {
+        "column": variable.variableName,
+        "termType": "http://www.w3.org/ns/r2rml#IRI"
+      },
+      "predicate": `http://timbuctoo.com/${property}`,
+      "http://timbuctoo.com/mapping/existingTimbuctooVre": variable.targetExistingTimbuctooVre
+    }
+  } else {
+    return {
+      "objectMap": {
+        "column": variable.variableName
+      },
+      "predicate": `http://timbuctoo.com/${property}`
+    }
+  }
+}
